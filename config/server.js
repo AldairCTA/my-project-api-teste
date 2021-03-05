b0f7f923ddef021cf3ad602f5e864d9b238c4717
AldairCTA <cta.aldairsilva@gmail.com>
2021-03-05 11:30:57 -0300

    feat .env example

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '81129ca5ca15af0f8418a467adecd207'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fe651943-c8e3-46a4-9889-8ecbdea506fa'
+};
